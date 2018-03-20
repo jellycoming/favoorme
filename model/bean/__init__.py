281e7d179090ad70a272376429846bf396501590
jellycoming <jellycoming@gmail.com>
2018-03-20 16:39:55 +0800

    usage of SQLAlchemy and MVC DAO

--- model/bean/__init__.py
+++ model/bean/__init__.py
@@ -2,3 +2,11 @@
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+
+class BaseModel(Base):
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return "{}(id='{}')".format(type(self).__name__, getattr(self, 'id'))
--- model/bean/app.py
+++ model/bean/app.py
@@ -1,17 +1,11 @@
 # coding=utf-8
 from sqlalchemy import Column, Integer, String
-from model.bean import Base
+from model.bean import BaseModel
 
 
-class App(Base):
+class App(BaseModel):
     __tablename__ = 'entity_app'
 
     id = Column('id', Integer, primary_key=True, autoincrement=True)
     name = Column('name', String)
     ctime = Column('ctime', String)
-
-    def __init__(self):
-        pass
-
-    def __repr__(self):
-        return "{}(id='{}',name='{}')".format(type(self).__name__, self.id, self.name)
